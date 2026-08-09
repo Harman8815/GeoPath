e682228f61814b746c1e7a01ec2bc72decc46ba7
Harman8815 <harman88157@gmail.com>
2026-08-09 21:32:37 +0530

    fixed non scroll issue on home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       lang="en"
       className={"  h-full antialiased"}
     >
-      <body className="m-0 h-full w-full overflow-hidden bg-[#111] p-0 text-white">
+      <body className="m-0 h-full w-full bg-[#111] p-0 text-white">
         {children}
       </body>
     </html>
